9491e622fbb0889715c908a7b70cc2ac128dac89
Lurgic-error <glngwenya@gmail.com>
2022-04-05 15:49:00 +0300

    kimepop

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from 'url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(), VitePWA({
+    srcDir: 'src',
+    filename: 'sw.js',
+    manifest: {
+      name: "Kimepop PWA",
+      theme_color: "#c3c3c3"
+    },
+    workbox: {
+      sourcemap: true
+    },
+    strategies: 'injectManifest',
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true
+      /* other options */
+    },
+  })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
